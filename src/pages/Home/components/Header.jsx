e717d8085170f54cb1c6d2ff8490db0cd64d8738
“sheen2536” <afsheensajad5@gmail.com>
2024-08-12 19:56:01 +0530

    Header update.

--- src/pages/Home/components/Header.jsx
+++ src/pages/Home/components/Header.jsx
@@ -1,70 +1,51 @@
-import { useState } from 'react';
 import './Header.css';
 import '../../../styles/Home.css';
 import '../../../App.css';
 import logo from '../../../../public/logo.png';
 import bg1 from '../../../../public/Group 1.png';
+import { IoMdArrowDropdown } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState({ register: false, resources: false });
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const toggleDropdown = (dropdown) => {
-    setDropdownOpen((prevState) => ({
-      ...prevState,
-      [dropdown]: !prevState[dropdown],
-    }));
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <>
-      <div className='whole'>
-        <img src={bg1} alt='bg' className='background-image' />
+      <img src={bg1} alt='bg' className='background-image' />
 
-        <nav className='Navbar'>
-          <img src={logo} alt='Logo' className='logo' />
+      <nav className='Navbar'>
+        <img src={logo} alt='Logo' className='logo' />
 
-          <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+        <div className="hamburger-container" onClick={toggleMenu}>
+          <RxHamburgerMenu id="hamburger" alt="Menu" />
+        </div>
 
-          <ul className={`list ${isOpen ? 'show' : ''}`}>
-            <li><a href='/' className='Home'>Home</a></li>
-            <li><a href='/events' className='Home'>Events</a></li>
-            <li
-              className={dropdownOpen.register ? 'show' : ''}
-              onClick={() => toggleDropdown('register')}
-            >
-              <a href='/register' className='Home'>
-                Register
-                <span className="dropdown-icon">▼</span> 
-              </a>
-              <ul className='dropdown'>
-               
-              </ul>
-            </li>
-            <li
-              className={dropdownOpen.resources ? 'show' : ''}
-              onClick={() => toggleDropdown('resources')}
-            >
-              <a href='/resources' className='Home'>
-                Resources
-                <span className="dropdown-icon">▼</span> 
-              </a>
-              <ul className='dropdown'>
-                
-              </ul>
-            </li>
-            <li><a href='/contact' className='Home'>Contact</a></li>
-          </ul>
-        </nav>
-      </div>
+        <ul className={`nav-links ${isMenuOpen ? "show" : ""}`}>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/events'>Events</a></li>
+          <li><a href='/sponsor'>Sponsor</a></li>
+          <li><a href='/register'>Register <IoMdArrowDropdown className='dropbtn'/></a>
+            <ul id='submenu'>
+              <li><a href="/attendee">Attendee</a></li>
+              <li><a href="/participant">Participant</a></li>
+              <li><a href="/institute">Institute</a></li>
+              <li><a href="/campus_ambassador">Campus Ambassador</a></li>
+            </ul>
+          </li>
+          <li><a href='/resources'>Resources <IoMdArrowDropdown className='dropbtn'/></a>
+            <ul id='submenu'>
+              <li><a href="/schedule">Schedule</a></li>
+              <li><a href="/brochure">Brochure</a></li>
+            </ul>
+          </li>
+          <li><a href='/contact'>Contact</a></li>
+        </ul>
+      </nav>
     </>
   );
 }
